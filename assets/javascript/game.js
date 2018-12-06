689d9dfd9c7d9e43ad783f99334faeb5120879e8
Joe Zufelt <joe.zufelt@gmail.com>
2018-12-06 18:26:01 -0500

    final before css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ var wins = 0;
 var losses = 0;
 var guesses = 9;
 var compGuess = [];
+var userGuessA = [];
 
 // HTML variables
 var textWins = document.getElementById("textWins");
@@ -15,18 +16,27 @@ var yourGuesses = document.getElementById("yourGuesses");
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
+    if (userGuessA.includes(userGuess) === false) {
     var compGuess = letters[Math.floor(Math.random() * letters.length)];
+
+    userGuessA.push(userGuess);
 
     if (userGuess === compGuess) {
         wins++;
         guesses = 9;
+        userGuessA = [];
     } else {
         guesses--;
-        
+        if (guesses === 0) {
+            losses++;
+            userGuessA = [];
+            guesses = 9;
+        }
     }
     
     textWins.textContent = "Wins: " + wins;
     textLosses.textContent = "Losses: " + losses;
     textGuessesLeft.textContent = "Guesses left: " + guesses;
-    yourGuesses.textContent = "Your guesses: " + userGuess;
+    yourGuesses.textContent = "Your guesses: " + userGuessA;
+    }
 };
